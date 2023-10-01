5a1bbbea98e2a7a9624f4da757dfab026289adce
Matt B <110642574+BeitM@users.noreply.github.com>
2023-10-01 13:29:11 -0400

    Revert "Merge branch 'master' of https://github.com/Terabridges/Center-Stage"
    This reverts commit be5909352def7d5a0e9d1873b04572c36f1b0d50, reversing
    changes made to 380e12369535acb5b82a7b3890d05f026c3a890e.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Drivetrain.java
@@ -1,18 +1,15 @@
 package org.firstinspires.ftc.teamcode.testing;
 
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Drivetrain {
     private DcMotor fl;
     private DcMotor fr;
     private DcMotor bl;
     private DcMotor br;
-    private double transScl = 1./3.;
-    private double rotScl = 1./3.;
+    private double transScl;
+    private double rotScl;
 
     Drivetrain(){
         fl = null;
@@ -37,72 +34,31 @@ public class Drivetrain {
         this.bl = bl;
         this.br = br;
     }
-    void setMotors(HardwareMap hardwareMap){
+    void setMotors(OpMode opMode){
         setMotors(
-                hardwareMap.get(DcMotor.class, "leftfront_drive"),
-                hardwareMap.get(DcMotor.class, "rightfront_drive"),
-                hardwareMap.get(DcMotor.class, "leftback_drive"),
-                hardwareMap.get(DcMotor.class, "rightback_drive")
+                opMode.hardwareMap.get(DcMotor.class, "leftfront_drive"),
+                opMode.hardwareMap.get(DcMotor.class, "rightfront_drive"),
+                opMode.hardwareMap.get(DcMotor.class, "leftback_drive"),
+                opMode.hardwareMap.get(DcMotor.class, "rightback_drive")
         );
-        setDirectionsStandard();
-    }
-    void setDirections(DcMotorSimple.Direction fl, DcMotorSimple.Direction fr, DcMotorSimple.Direction bl, DcMotorSimple.Direction br){
-        this.fl.setDirection(fl);
-        this.fr.setDirection(fr);
-        this.bl.setDirection(bl);
-        this.br.setDirection(br);
-    }
-    void setDirectionsStandard(){
-        setDirections(
-                DcMotorSimple.Direction.REVERSE,
-                DcMotorSimple.Direction.FORWARD,
-                DcMotorSimple.Direction.REVERSE,
-                DcMotorSimple.Direction.FORWARD
-        );
-    }
-
-    DcMotor getFl() {
-        return fl;
-    }
-    DcMotor getFr() {
-        return fr;
-    }
-    DcMotor getBl() {
-        return bl;
-    }
-    DcMotor getBr() {
-        return br;
     }
 
     void motorPow(double fl, double fr, double bl, double br){
-        double max = Math.max(
-                1.,
-                Math.max(
-                        Math.max(
-                                fl,
-                                fr
-                        ),
-                        Math.max(
-                                bl,
-                                br
-                        )
-                )
-        );
-        this.fl.setPower(fl/max);
-        this.fr.setPower(fr/max);
-        this.bl.setPower(bl/max);
-        this.br.setPower(br/max);
+        this.fl.setPower(fl);
+        this.fr.setPower(fr);
+        this.bl.setPower(bl);
+        this.br.setPower(br);
     }
     void transRotPow(double forward, double side, double rot){
         motorPow(
-                (forward - side)*transScl - rot*rotScl,
-                (forward + side)*transScl + rot*rotScl,
                 (forward + side)*transScl - rot*rotScl,
-                (forward - side)*transScl + rot*rotScl
+                (forward - side)*transScl + rot*rotScl,
+                (forward - side)*transScl - rot*rotScl,
+                (forward + side)*transScl + rot*rotScl
         );
     }
     void absoluteTransRotPow(double robotDir, double forward, double side, double rot){
-        // Visualization of why this works: https://www.desmos.com/calculator/w7o0jrr7fq
+        // Visualization of why this works: https://www.desmos.com/calculator/kgy249h8rj
         transRotPow(
                 forward*Math.cos(robotDir) + side*Math.sin(robotDir),
                 forward*Math.cos(robotDir + Math.PI/2) + side*Math.sin(robotDir + Math.PI/2),
